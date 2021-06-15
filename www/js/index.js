6e12af129134401456d8be3765b48ae44b90f886
Caleb Trachte <cetrachte@hotmail.com>
2021-06-15 09:15:03 -0500

    add cordova plugin info

--- www/js/index.js
+++ www/js/index.js
@@ -24,5 +24,5 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    console.log('Running cordova-' + device.platform + " - " + cordova.platformId + '@' + cordova.version);
 }
